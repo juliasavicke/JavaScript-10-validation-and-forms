ab2acf85a3538eb9c1b9774af970072e9af98a3d
juliasavicke <juliasavicke@gmail.com>
2022-10-21 15:59:02 +0300

    the end

--- js/uzd.js
+++ js/uzd.js
@@ -2,52 +2,50 @@
 console.log("uzd.js");
 
 const formEl = document.getElementById("actionForm");
-const emailEl = document.getElementById("email");
-const passwordEl = document.getElementById("psw");
-const passwordRepeatEl = document.getElementById("psw-repeat");
+const emailEl = formEl.elements.email;
+const passwordEl = formEl.elements.psw;
+const passwordRepeatEl = formEl.elements.pswRepeat;
 
 formEl.addEventListener("submit", (event) => {
   event.preventDefault();
-  console.log("Submit ivyko, valdymas perimtas");
   clearErrors();
-
-  const inputValues = {};
-
-  inputValues.emailEl = emailEl.value.trim();
-  inputValues.passwordEl = passwordEl.value.trim();
-  inputValues.passwordRepeatEl = passwordRepeatEl.value.trim();
-
-  console.log("inputValues ===", inputValues);
-  console.log("email ===", formEl.elements.email.value);
-  console.log("email ===", formEl.elements.psw.value);
-  //console.log("email ===", formEl.elements.psw - repeat.value);
-
-  //elementai formoje:
-  //formEl.elements.email.value;
-
   if (
     checkIfEmpty(emailEl) &&
+    isThereALetter(emailEl.value, "@") &&
     checkIfEmpty(passwordEl) &&
     checkIfEmpty(passwordRepeatEl) &&
     checkPasswords(passwordEl, passwordRepeatEl)
   ) {
     console.log("Viskas uzpildyta");
     formEl.style.display = "none";
-    const h2El = document.createElement("h2");
-    h2El.textContent = emailEl.value;
-    const pEl = document.createElement("p");
-    pEl.textContent = passwordEl.value;
-    document.body.append(h2El, pEl);
+    createEl("h2", emailEl.value);
+    createEl("pre", passwordEl.value);
   }
 });
 
+function isThereALetter(valueToCheck, needle) {
+  console.log("valueToCheck ===", valueToCheck);
+  console.log("needle ===", needle);
+  console.log(
+    "valueToCheck.includes(needle) ===",
+    valueToCheck.includes(needle)
+  );
+  return valueToCheck.includes(needle);
+}
+
+function createEl(tagName, content) {
+  const el = document.createElement(tagName);
+  el.textContent = content;
+  document.body.append(el);
+}
+
 function checkIfEmpty(el) {
-  if (el.value.length === 0) {
+  if (el.value.trim().length === 0) {
     const prevEl = el.previousElementSibling.innerHTML;
     el.nextElementSibling.style.display = "block";
     el.nextElementSibling.textContent = `${prevEl} neuzpildytas`;
     return false;
-  } else if (el.value.length <= 3) {
+  } else if (el.value.trim().length <= 3) {
     const prevEl = el.previousElementSibling.innerHTML;
     el.nextElementSibling.style.display = "block";
     el.nextElementSibling.textContent = `Iveskite 4 ar daugiau simboliu`;
@@ -56,7 +54,7 @@ function checkIfEmpty(el) {
 }
 
 function checkPasswords(psw, pswRepeat) {
-  if (psw.value != pswRepeat.value) {
+  if (psw.value !== pswRepeat.value) {
     psw.nextElementSibling.style.display = "block";
     psw.nextElementSibling.textContent = `${psw.previousElementSibling.innerHTML} ir ${pswRepeat.previousElementSibling.innerHTML} nesutampa`;
     return false;
